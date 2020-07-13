dc830f72a9d388d56679ae75651de00be37939ed
ehei1 <ehei@naver.com>
2020-07-13 17:57:05 +0900

    async processing

--- ImageFilter/ImageFilter.cpp
+++ ImageFilter/ImageFilter.cpp
@@ -3,7 +3,9 @@
 #include <cassert>
 #include <chrono>
 #include <string>
+#include <vector>
 
+#include <d3d9.h>
 #include <Shlwapi.h>
 
 #define HAVE_OPENCV
@@ -14,46 +16,45 @@
 #include "ImageFilter.h"
 
 
-
-
 namespace DotEngine
 {
+	Token::Index ImageFilter::Task::_unique_index{};
+
+
+	Token::~Token()
+	{
+		if (_valid) {
+			_imageFilter._remove_task(_index);
+		}
+	}
+
 	class Waifu2xFilterImpl
 	{
-		W2XConv* _converter{};
-		const int _denoise_level = 3;
+		std::vector<W2XConv*> _converters{ nullptr, nullptr, nullptr, nullptr };
 
 	public:
-		Waifu2xFilterImpl::Waifu2xFilterImpl() : _converter{ w2xconv_init(W2XConvGPUMode::W2XCONV_GPU_AUTO, 1, 0) }
-		{
-			TCHAR filePath[MAX_PATH]{};
-			GetModuleFileName(NULL, filePath, _countof(filePath));
-
-			PathRemoveFileSpec(filePath);
-			PathAppend(filePath, TEXT("models_rgb"));
-
-			if (auto error = w2xconv_load_model(_denoise_level, _converter, filePath)) {
-				assert(false);
-
-				_check_for_errors(_converter, error);
-				throw std::invalid_argument("invalid model path");
-			}
-		}
+		Waifu2xFilterImpl::Waifu2xFilterImpl() 
+		{}
 
 		Waifu2xFilterImpl::~Waifu2xFilterImpl()
 		{
-			w2xconv_fini(_converter);
+			auto destory_converter = [](W2XConv* converter) { 
+				if (converter) { 
+					w2xconv_fini(converter); 
+				} 
+			};
+			std::for_each(std::begin(_converters), std::end(_converters), destory_converter);
 		}
 
-		HRESULT filter(LPVOID pBits, size_t width, size_t height, bool has_alpha) const
+		HRESULT filter(LPVOID pBits, size_t width, size_t height, bool has_alpha, int denoise_level, float scale)
 		{
-			double scale = 1.f;
 			int block_size{};
+			auto converter{ _get_converter(denoise_level) };
 
-			if (auto error = w2xconv_convert_memory(_converter, width, height, pBits, _denoise_level, scale, block_size, has_alpha, CV_8UC4)) {
+			if (auto error = w2xconv_convert_memory(converter, width, height, pBits, denoise_level, scale, block_size, has_alpha, CV_8UC4)) {
 				assert(false);
 
-				_check_for_errors(_converter, error);
+				_check_for_errors(converter, error);
 				return E_FAIL;
 			}
 
@@ -61,6 +62,34 @@ namespace DotEngine
 		}
 
 	private:
+		W2XConv* _get_converter(int denoise_level)
+		{
+			assert(static_cast<int>(_converters.size()) > denoise_level);
+
+			auto& converter = _converters[denoise_level];
+
+			if (!converter) {
+				auto job_number = 1;
+				auto log_level = 0; // [0,4]
+				converter = w2xconv_init(W2XConvGPUMode::W2XCONV_GPU_AUTO, job_number, log_level);
+
+				TCHAR filePath[MAX_PATH]{};
+				GetModuleFileName(NULL, filePath, _countof(filePath));
+
+				PathRemoveFileSpec(filePath);
+				PathAppend(filePath, TEXT("models_rgb"));
+
+				if (auto error = w2xconv_load_model(denoise_level, converter, filePath)) {
+					assert(false);
+
+					_check_for_errors(converter, error);
+					throw std::invalid_argument("invalid model path");
+				}
+			}
+
+			return converter;
+		}
+
 		void _check_for_errors(W2XConv* converter, int error) const
 		{
 			if (error)
@@ -73,6 +102,17 @@ namespace DotEngine
 		}
 
 	};
+
+	void ImageFilter::Task::start(DirectX::ScratchImage&& image)
+	{
+		if (_async_started) {
+			throw std::runtime_error("async job started already");
+		}
+		else {
+			_future = std::async(std::launch::async, _function, std::move(image));
+			_async_started = true;
+		}
+	}
 
 
 	ImageFilter::ImageFilter() : _impl{ new Waifu2xFilterImpl }
@@ -103,7 +143,7 @@ namespace DotEngine
 
 				static_assert(sizeof(WORD) == 2, "invalid size");
 
-				copyFromSurfaceMemory(highColorImage.GetPixels(), srcLockedRect.pBits, width, height, srcLockedRect.Pitch, 16);
+				__copy_from_surface_memory(highColorImage.GetPixels(), srcLockedRect.pBits, width, height, srcLockedRect.Pitch, 16);
 
 				//DirectX::SaveToDDSFile(*highColorImage.GetImage(0, 0, 0), 0, std::wstring(L"D:\\study\\im.high.dds").c_str());
 
@@ -121,13 +161,13 @@ namespace DotEngine
 
 				//DirectX::SaveToDDSFile(*trueColorImage.GetImage(0, 0, 0), 0, std::wstring(L"D:\\study\\im.true.dds").c_str());
 
-				if (FAILED(_impl->filter(trueColorImage.GetPixels(), width, height, has_alpha))) {
+				if (FAILED(_impl->filter(trueColorImage.GetPixels(), width, height, has_alpha, 3, 1.f))) {
 					return E_FAIL;
 				}
 
 				auto filteringTime = getTime() - curTime;
 
-				copyToSurfaceMemory(dstLockedRect.pBits, trueColorImage.GetPixels(), width, height, dstLockedRect.Pitch, 32);
+				__copy_to_surface_memory(dstLockedRect.pBits, trueColorImage.GetPixels(), width, height, dstLockedRect.Pitch, 32);
 
 				std::string name = std::to_string(width) + "x" + std::to_string(height);
 				std::string log = name + "\t" + std::to_string(conversionTime.count()) + "\t" + std::to_string(filteringTime.count()) + "\n";
@@ -156,7 +196,7 @@ namespace DotEngine
 				DirectX::ScratchImage highColorImage;
 				highColorImage.Initialize2D(DXGI_FORMAT_B4G4R4A4_UNORM, width, height, 1, 1);
 
-				copyFromSurfaceMemory(highColorImage.GetPixels(), lockedRect.pBits, width, height, lockedRect.Pitch, 16);
+				__copy_from_surface_memory(highColorImage.GetPixels(), lockedRect.pBits, width, height, lockedRect.Pitch, 16);
 
 				auto format = (has_alpha ? DXGI_FORMAT_B8G8R8A8_UNORM : DXGI_FORMAT_B8G8R8X8_UNORM);
 				DirectX::ScratchImage trueColorImage;
@@ -167,7 +207,7 @@ namespace DotEngine
 					return E_FAIL;
 				}
 
-				if (FAILED(_impl->filter(trueColorImage.GetPixels(), width, height, has_alpha))) {
+				if (FAILED(_impl->filter(trueColorImage.GetPixels(), width, height, has_alpha, 3, 1.f))) {
 					return E_FAIL;
 				}
 
@@ -176,7 +216,7 @@ namespace DotEngine
 					return E_FAIL;
 				}
 
-				copyToSurfaceMemory(lockedRect.pBits, trueColorImage.GetPixels(), width, height, lockedRect.Pitch, 16);
+				__copy_to_surface_memory(lockedRect.pBits, trueColorImage.GetPixels(), width, height, lockedRect.Pitch, 16);
 			}
 			// 32bpp 그림은 디더링되지 않은 것으로 간주하고 필터 처리도 하지 않는다
 		case D3DFMT_A8R8G8B8:
@@ -190,7 +230,137 @@ namespace DotEngine
 		return S_OK;
 	}
 
-	void ImageFilter::copyFromSurfaceMemory(LPVOID pDst, LPVOID pSrc, size_t width, size_t height, UINT pitch, UINT bitPerPixel) const
+	std::shared_ptr<Token> ImageFilter::filter_async(LPDIRECT3DTEXTURE9 pSrcTexture, LPDIRECT3DTEXTURE9 pDstTexture, int denoise_level, float scale, Callback_type callback)
+	{
+		D3DSURFACE_DESC surface_desc{};
+		
+		if (FAILED(pSrcTexture->GetLevelDesc(0, &surface_desc))) {
+			throw std::runtime_error("D3DSURFACE_DESC getting is failed");
+		}
+
+		bool has_alpha{};
+
+		switch (surface_desc.Format) {
+		case D3DFMT_A4R4G4B4:
+			has_alpha = true;
+		case D3DFMT_X4R4G4B4:
+		{
+			auto functor = std::bind(&ImageFilter::__apply_waifu2x_async, this, surface_desc.Width, surface_desc.Height, has_alpha, denoise_level, scale, std::placeholders::_1);
+			auto task = std::make_shared<Task>(pSrcTexture, pDstTexture, functor, callback);
+			_tasks[task->_index] = task;
+			_task_indices.push(task->_index);
+
+			return task->issue_token(*this);
+		}	
+		case D3DFMT_A8B8G8R8:
+		case D3DFMT_X8B8G8R8:
+			return nullptr;
+		default:
+			throw std::invalid_argument("some formats do not support");
+		}
+	}
+
+	void ImageFilter::update()
+	{
+		if (_task_indices.empty()) {
+			return;
+		}
+
+		auto task_index = _task_indices.front();
+		auto task_iter = _tasks.find(task_index);
+
+		// 이미 취소된 작업인다
+		if (std::end(_tasks) == task_iter) {
+			_task_indices.pop();
+			return;
+		}
+
+		auto task = task_iter->second;
+
+		// 시작된 작업
+		if (task->_async_started) {
+			// 작업 끝
+			if (std::future_status::ready == task->_future.wait_until(std::chrono::system_clock::now())) {
+				auto trueColorImage = task->_future.get();
+
+				{
+					D3DLOCKED_RECT locked_rect{};
+					
+					if (SUCCEEDED(task->_pDstTexture->LockRect(0, &locked_rect, NULL, 0))) {
+						D3DSURFACE_DESC surface_desc{};
+						
+						if (SUCCEEDED(task->_pDstTexture->GetLevelDesc(0, &surface_desc))) {
+							__copy_to_surface_memory(locked_rect.pBits, trueColorImage.GetPixels(), surface_desc.Width, surface_desc.Height, locked_rect.Pitch, 32);
+							
+							task->_callback(task->_pDstTexture);
+						}
+					}
+				}
+
+				_task_indices.pop();
+				_tasks.erase(task_index);
+			}
+		}
+		// 작업을 시작시킨다
+		else {
+			assert(false == task->_cancelled);
+
+			D3DLOCKED_RECT locked_rect{};
+			constexpr UINT mipmap_level{};
+
+			if (SUCCEEDED(task->_pSrcTexture->LockRect(mipmap_level, &locked_rect, NULL, 0))) {
+				D3DSURFACE_DESC surface_desc{};
+
+				if (SUCCEEDED(task->_pSrcTexture->GetLevelDesc(mipmap_level, &surface_desc))) {
+					// memory copy
+					DirectX::ScratchImage highColorImage;
+					{
+						highColorImage.Initialize2D(DXGI_FORMAT_B4G4R4A4_UNORM, surface_desc.Width, surface_desc.Height, 1, 1);
+
+						__copy_from_surface_memory(highColorImage.GetPixels(), locked_rect.pBits, surface_desc.Width, surface_desc.Height, locked_rect.Pitch, 16);
+						task->_pSrcTexture->UnlockRect(mipmap_level);
+					}
+
+					task->start(std::move(highColorImage));
+				}
+			}
+		}
+	}
+
+	void ImageFilter::_remove_task(Token::Index index)
+	{
+		auto task_iter = _tasks.find(index);
+
+		if (std::end(_tasks) != task_iter) {
+			auto& task = task_iter->second;
+
+			// 비동기 작업이 시작된 상태에서 future를 소멸시키면 처리가 봉쇄된다. 결과 대기를 DirectX 장치가 생성된 스레드에서 해야한다. 따라서 이런 행위는 프레임 멈춤을 유발한다. 이를 우회하기 위해 한번 시작된 작업은 끝날 때까지 봐준다.
+			if (task->_async_started) {
+				task_iter->second->_cancelled = true;
+			}
+			else {
+				_tasks.erase(task_iter);
+			}
+		}
+	}
+
+	DirectX::ScratchImage ImageFilter::__apply_waifu2x_async(size_t width, size_t height, bool has_alpha, int denoise_level, float scale, DirectX::ScratchImage&& highColorImage)
+	{
+		auto format = (has_alpha ? DXGI_FORMAT_B8G8R8A8_UNORM : DXGI_FORMAT_B8G8R8X8_UNORM);
+		DirectX::ScratchImage trueColorImage;
+
+		// 트루 컬러 그림으로 바꾼다
+		if (FAILED(DirectX::Convert(highColorImage.GetImages(), highColorImage.GetImageCount(), highColorImage.GetMetadata(), format, DirectX::TEX_FILTER_FLAGS::TEX_FILTER_DEFAULT, DirectX::TEX_THRESHOLD_DEFAULT, trueColorImage))) {
+			throw std::runtime_error("image conversion is failed");
+		}
+		else if (FAILED(_impl->filter(trueColorImage.GetPixels(), width, height, has_alpha, denoise_level, scale))) {
+			throw std::runtime_error("image conversion is failed");
+		}
+
+		return trueColorImage;
+	}
+
+	void ImageFilter::__copy_from_surface_memory(LPVOID pDst, LPVOID pSrc, size_t width, size_t height, UINT pitch, UINT bitPerPixel) const
 	{
 		auto byteSize = bitPerPixel / 8;
 		auto recordSize = byteSize * width;
@@ -204,7 +374,7 @@ namespace DotEngine
 		}
 	}
 
-	void ImageFilter::copyToSurfaceMemory(LPVOID pDst, LPVOID pSrc, size_t width, size_t height, UINT pitch, UINT bitPerPixel) const
+	void ImageFilter::__copy_to_surface_memory(LPVOID pDst, LPVOID pSrc, size_t width, size_t height, UINT pitch, UINT bitPerPixel) const
 	{
 		auto byteSize = bitPerPixel / 8;
 		auto recordSize = byteSize * width;
--- ImageFilter/ImageFilter.h
+++ ImageFilter/ImageFilter.h
@@ -1,40 +1,115 @@
 #pragma once
 
+#include <functional>
 #include <future>
+#include <queue>
 #include <memory>
+#include <unordered_map>
 #include <winnt.h>
 #include <d3d9types.h>
+#include <d3d9.h>
 
+
+namespace DirectX {
+	class ScratchImage;
+}
 
 
 namespace DotEngine
 {
 	class Waifu2xFilterImpl;
+	class ImageFilter;
 
 	struct Token
 	{
-		size_t _index{};
+		using Index = size_t;
+		ImageFilter& _imageFilter;
+		Index const _index{};
+		bool _valid{ true };
 
-		Token(size_t index) : _index{ index }
+		Token(ImageFilter& imageFilter, Index index) : _imageFilter{ imageFilter }, _index{ index }
 		{}
+		~Token();
+
+		inline void invalidate() { _valid = false; }
 	};
 
 	class ImageFilter
 	{
 		friend Token;
+		using Callback_type = std::function<void(LPDIRECT3DTEXTURE9)>;
+
+		struct Task
+		{
+			using Function_type = std::function<DirectX::ScratchImage(DirectX::ScratchImage&&)>;
+			Function_type _function;
+			Callback_type _callback;
+
+			LPDIRECT3DTEXTURE9 _pSrcTexture{};
+			LPDIRECT3DTEXTURE9 _pDstTexture{};
+			Token::Index const _index;
+			static Token::Index _unique_index;
+
+			std::future<DirectX::ScratchImage> _future;
+			std::weak_ptr<Token> _weak_token_ptr;
+			bool _cancelled{};
+			bool _token_issued{};
+			bool _async_started{};
+
+			Task(LPDIRECT3DTEXTURE9 pSrcTexture, LPDIRECT3DTEXTURE9 pDstTexture, Function_type function, Callback_type callback) : _pSrcTexture{ pSrcTexture }, _pDstTexture{ pDstTexture }, _function{ function }, _callback{ callback }, _index { ++_unique_index }
+			{}
+
+			~Task() 
+			{
+				if (_token_issued) {
+					if (auto token_ptr = _weak_token_ptr.lock()) {
+						token_ptr->invalidate();
+					}
+				}
+			}
+
+			std::shared_ptr<Token> issue_token(ImageFilter& imageFilter) 
+			{
+				if (_token_issued) {
+					throw std::runtime_error("token issued already");
+				}
+
+				auto shared_ptr = std::make_shared<Token>(imageFilter, _index);
+				_weak_token_ptr = shared_ptr;
+				_token_issued = true;
+
+				return shared_ptr;
+			}
+
+			void start(DirectX::ScratchImage&&);
+		};
 
 		std::unique_ptr< Waifu2xFilterImpl > _impl;
+		std::queue<Token::Index> _task_indices;
+		std::unordered_map<Token::Index, std::shared_ptr<Task>> _tasks;
 
 	public:
 		ImageFilter();
 		~ImageFilter();
 
-		HRESULT applyWaifu2x(D3DLOCKED_RECT& lockedRect, size_t width, size_t height, D3DFORMAT) const;
-		
+		HRESULT applyWaifu2x(D3DLOCKED_RECT& lockedRect, size_t width, size_t height, D3DFORMAT) const;		
 		HRESULT applyWaifu2x(const D3DLOCKED_RECT& srcLockedRect, D3DLOCKED_RECT& dstLockedRect, size_t width, size_t height, D3DFORMAT) const;
 
+		// 필터링을 시작한다
+		std::shared_ptr<Token> filter_async(LPDIRECT3DTEXTURE9 pSrcTexture, LPDIRECT3DTEXTURE9 pDstTexture, int denoise_level, float scale, Callback_type);
+
+		// 비동기 처리가 완료되었는지 엿보기 위해 매 프레임 호출되어야 한다
+		void update();
+
+	protected:
+		void _remove_task(Token::Index index);
+
 	private:
-		void copyFromSurfaceMemory(LPVOID pDst, LPVOID pSrc, size_t width, size_t height, UINT pitch, UINT bitPerPixel) const;
-		void copyToSurfaceMemory(LPVOID pDst, LPVOID pSrc, size_t width, size_t height, UINT pitch, UINT bitPerPixel) const;
+		void __copy_from_surface_memory(LPVOID pDst, LPVOID pSrc, size_t width, size_t height, UINT pitch, UINT bitPerPixel) const;
+		void __copy_to_surface_memory(LPVOID pDst, LPVOID pSrc, size_t width, size_t height, UINT pitch, UINT bitPerPixel) const;
+
+		DirectX::ScratchImage __apply_waifu2x_async(size_t width, size_t height, bool has_alpha, int denoise_level, float scale, DirectX::ScratchImage&&);
 	};
 }
+
+// TODO: remove token-task frame
